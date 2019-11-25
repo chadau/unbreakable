b2e5a56e4a28007348c22b255ac6c61991de64b1
Lucas Cheminade <lucas.cheminade@epitech.eu>
2019-11-25 22:07:38 +0200

    last push

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { LoginComponent } from './../login/login.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AuthPageRoutingModule } from './auth-routing.module';
+
+import { AuthPage } from './auth.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    AuthPageRoutingModule
+  ],
+  declarations: [AuthPage, LoginComponent]
+})
+export class AuthPageModule {}
